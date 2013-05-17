79369ed523593483ed44edaf847fedda1a3d85db
Bunny Yiu <bunny116@gmail.com>
2013-05-18 01:15:45 +0800

    fix can't install in Mac

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 
 currPath="$( cd "$( dirname "$0" )" && pwd)"
-ln -sf $currPath/.vimrc $HOME --backup
-ln -sf $currPath/.vim $HOME --backup
+os=`uname`
+backup_opt="--backup"
+if [[ $os == "Darwin" ]]; then
+  #no --backup option in Mac
+  backup_opt=""
+fi
+ln -sf $currPath/.vimrc $HOME $backup_opt
+ln -sf $currPath/.vim $HOME $backup_opt
 git submodule update --init --recursive
 git submodule foreach git checkout master
 echo "run BundleUpdate in vim to update bundled modules"
