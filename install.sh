52101fccbf8d4c68b52dec88a8dac9ee4afb3523
Bunny Yiu <bunnyyiu@clustertech.com>
2013-05-17 16:52:02 +0000

    checkout master branch in submodules to enable BundleUpdate

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 currPath="$( cd "$( dirname "$0" )" && pwd)"
-ln -sf $currPath/.vimrc $HOME/.vimrc
-ln -sf $currPath/.vim $HOME/.vim
-echo "To update module, please run BundleUpdate"
+ln -sf $currPath/.vimrc $HOME --backup
+ln -sf $currPath/.vim $HOME --backup
+git submodule update --init --recursive
+git submodule foreach git checkout master
+echo "run BundleUpdate in vim to update bundled modules"
